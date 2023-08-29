4021c59177ea5115118ffdbe2a2487c1bab3152c
SebMO <sebasto7@users.noreply.github.com>
2023-08-29 15:22:19 +0200

    Adding whitin and betweencosine similarities comparisons

--- variability-analysis/variability-postanalysis-main.py
+++ variability-analysis/variability-postanalysis-main.py
@@ -24,12 +24,11 @@ import seaborn as sns
 #%% Custom functions 
 #TODO load this functions from a helper file at some point
 
-def cosine_similarity_and_clustering(_data):
+def cosine_similarity_and_clustering(_data,cosine_subgroups):
     import numpy as np
     import pandas as pd
     from sklearn.metrics.pairwise import cosine_similarity
     from scipy.cluster import hierarchy
-
     # Filtering out columns with no data
     dropped_indexes = []
     kept_indexes = []
@@ -37,13 +36,35 @@ def cosine_similarity_and_clustering(_data):
     dropped_indexes.extend(list(set(_data.index) - set(dropped_data.index)))
     kept_indexes.extend(dropped_data.index)
     print(f'Dropping {len(dropped_indexes)} Tm9 columns with no data during cosine_sim analysis')
-
     _data.dropna(how='all', inplace=True)  # now dropping if all values in the row are nan
+
+    #Doing cosine similarities in subgroups in the data set
+    # Separate data into subgroups based on subgroup letters in the index
+    subgroup_data = {}
+    for subgroup in cosine_subgroups:
+        subgroup_data[subgroup] = _data[_data.index.str.contains(subgroup)]
+
+    # Calculate cosine similarity within each subgroup
+    cos_sim_within = {}
+    cos_sim_within_medians = {}
+    for subgroup, subgroup_df in subgroup_data.items():
+        cos_sim_within[subgroup] = cosine_similarity(subgroup_df.fillna(0))
+        cos_sim_within_medians[subgroup] = list(np.round(np.nanmedian(cos_sim_within[subgroup], 1), 2)) # pulling values together for each postsynaptic neuron
+
+    # Calculate cosine similarity between subgroups if needed
+    cos_sim_between = cosine_similarity(subgroup_data[cosine_subgroups[0]].fillna(0), subgroup_data[cosine_subgroups[1]].fillna(0))
+    cos_sim_between_medians = list(np.round(np.nanmedian(cos_sim_between, 1), 2)) # pulling values together for each postsynaptic neuron
+
+    # Within and between together in a dictionary
+    cos_sim_medians = cos_sim_within_medians
+    cos_sim_medians[''.join(cosine_subgroups)] = cos_sim_between_medians
+
     _data.fillna(0, inplace=True)  # Filling the remaining absent connectivity with a meaningful zero
 
     # Calculate cosine similarity
     cosine_sim = cosine_similarity(_data.values)
     cosine_sim_df = pd.DataFrame(cosine_sim, index=_data.index, columns=_data.index)
+
 
     hemisphere_list = [index_name.split(':')[2][0] for index_name in _data.index]
     d_v_list = [index_name.split(':')[3] for index_name in _data.index]
@@ -52,11 +73,12 @@ def cosine_similarity_and_clustering(_data):
     cosine_sim_summary_df = pd.DataFrame(columns=['cosine_sim', 'dorso-ventral', 'hemisphere','neuron'],
                                          index=_data.index.tolist())
     cosine_sim_nan = np.where(cosine_sim == 1., np.nan, cosine_sim)
-    cosine_sim_list = np.round(np.nanmedian(cosine_sim_nan, 1), 2)
+    cosine_sim_list = np.round(np.nanmedian(cosine_sim_nan, 1), 2) # pulling values together for each postsynaptic neuron
     cosine_sim_summary_df['cosine_sim'] = cosine_sim_list
     cosine_sim_summary_df['hemisphere'] = hemisphere_list
     cosine_sim_summary_df['dorso-ventral'] = d_v_list
     cosine_sim_summary_df['neuron'] = cell_type_list
+
 
     dendrogram_cosine = hierarchy.linkage(cosine_sim, method='ward')
     cosine_row_order = hierarchy.leaves_list(dendrogram_cosine)
@@ -68,7 +90,7 @@ def cosine_similarity_and_clustering(_data):
                                           index=_data_reordered_cosine_sim.index,
                                           columns=_data_reordered_cosine_sim.index)
 
-    return cosine_sim_df, cosine_sim_summary_df, cosine_row_order, dendrogram_cosine, cosine_sim_reordered_df, _data_reordered_cosine_sim, cosine_sim,cosine_sim_reordered
+    return cosine_sim_df, cosine_sim_summary_df, cosine_row_order, dendrogram_cosine, cosine_sim_reordered_df, _data_reordered_cosine_sim, cosine_sim, cosine_sim_reordered, cos_sim_medians
 
 
 #%% 
@@ -81,7 +103,16 @@ dataPath =  f'{PC_disc}:\Connectomics-Data\FlyWire\Processed-data'
 
 
 # Comparisons (between processed-data)
-data_frames_to_compare_ls = ['Tm1_FAFB_R__Absolut_counts', 'Tm2_FAFB_R__Absolut_counts', 'Tm9_FAFB_R__Absolut_counts']
+data_frames_to_compare_ls = ['Tm9_FAFB_R_'] # 'Tm9_FAFB_L_R_'], ['Tm9_FAFB_R_', 'Tm1_FAFB_R_','Tm2_FAFB_R_']
+user_defined_categoriers = ['Tm9_R'] # ['Tm9_R', 'Tm1_R', 'Tm2_R']
+cosine_subgroups = ['D', 'V'] # ['D', 'V'], ['R', 'L']
+
+
+excel_file_to_load = []
+for file_name in data_frames_to_compare_ls:
+    file_name = file_name + '.xlsx'
+    excel_file_to_load.append(os.path.join(dataPath,file_name))
+
 
 #%% 
 ######################################## LOADING PREPROCESSED DATA ############################################
@@ -94,7 +125,7 @@ excel_files = glob.glob(os.path.join(dataPath , '*.xlsx'))
 data_frames = {}
 
 # Iterate through each Excel file
-for excel_file in excel_files:
+for excel_file in excel_file_to_load:
     # Get the distinct part of the filename (excluding extension)
     file_name = os.path.splitext(os.path.basename(excel_file))[0]
     
@@ -122,19 +153,41 @@ for excel_file in excel_files:
 
 ############################################    Cosine similarity     #########################################
 
-# Computing cosine similarity
-data_frames_to_compare_ls = ['Tm1_FAFB_R__Absolut_counts', 'Tm2_FAFB_R__Absolut_counts', 'Tm9_FAFB_R__Absolut_counts']
+
+## For multiple data sets
+# Computing cosine similarity for absolute counts
 combined_cosine_sim_summary_df = pd.DataFrame()
-for df_name in data_frames_to_compare_ls:
+cos_sim_medians_dict = {}
+for i,df_name in enumerate(data_frames_to_compare_ls):
+    df_name = df_name + '_Absolut_counts'
     _data = data_frames[df_name]
     # Call the function and only define cosine_sim_summary_df
-    cosine_sim_summary_df = cosine_similarity_and_clustering(_data)[1]
+    cosine_sim_summary_df = cosine_similarity_and_clustering(_data,cosine_subgroups)[1]
+    cosine_sim_summary_df['neuron'] = user_defined_categoriers[i]
+    #Call the function and only define cos_sim_medians 
+    cos_sim_medians_dict[df_name] = cosine_similarity_and_clustering(_data,cosine_subgroups)[8]
 
     # Concatenate the current dataframe to the combined dataframe
     combined_cosine_sim_summary_df = pd.concat([combined_cosine_sim_summary_df, cosine_sim_summary_df])
 
 # Reset index of the combined dataframe
 combined_cosine_sim_summary_df = combined_cosine_sim_summary_df.reset_index(drop=True)
+
+
+## For single data set
+_dict = cos_sim_medians_dict[data_frames_to_compare_ls[0]+'_Absolut_counts']
+
+combined_cosine_sim_list = []
+combined_neuron_list = []
+for key,value in _dict.items():
+    combined_cosine_sim_list = combined_cosine_sim_list + value
+    combined_neuron_list = combined_neuron_list + [key]*len(value)
+
+# Create the dataframe with all subgropus
+combined_cosine_sim_summary_df = pd.DataFrame()
+combined_cosine_sim_summary_df['cosine_sim'] = combined_cosine_sim_list
+combined_cosine_sim_summary_df['neuron'] = combined_neuron_list
+
 
 
 
@@ -145,6 +198,8 @@ combined_cosine_sim_summary_df = combined_cosine_sim_summary_df.reset_index(drop
 
 
 ############################################    Cosine similarity     #########################################
+###########################################    Multiple Comparisons    ########################################
+
 data = combined_cosine_sim_summary_df.copy()
 
 ### Chekcing data distribution
@@ -217,17 +272,6 @@ for i, category1 in enumerate(categories):
 plt.ylim(plt.ylim()[0], max(line_positions.values()) + line_distance)
 
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
 
 
 print('Coding here')
